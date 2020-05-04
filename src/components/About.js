68a8874ce85dbbf538ac15a42fca9c0dc8d248f9
hquach-cs <hquach.cs@gmail.com>
2020-05-04 12:06:42 -0500

    Social Links list complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
 import "./../styles/about.css";
+import AboutCard from "./AboutCard";
 
 export default class About extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      welcome: {
+        class: "about-welcome",
+        title: "More about me!",
+        text:
+          "I'm an aspiring software developer that loves to program multiple interesting apps to showcase to users.",
+      },
+      goals: {
+        class: "about-goals",
+        title: "Goals",
+        text:
+          "My goal is to become a reliable programmer that create multiple things that users/consumers enjoy.",
+      },
+      education: {
+        class: "about-education",
+        title: "Education",
+        text:
+          "I got my associate and bachelor's degree in computer science. I graduated from The University of Tennessee, Knoxville in December of 2019. Go Vols!",
+      },
+      tbd: {
+        class: "about-TBD",
+        title: "TBD",
+        text:
+          "Integer vehicula justo vel sapien viverra congue. Etiam efficitur In scelerisque porta dictum. Suspendisse condimentum finibus eros,",
+      },
+    };
+  }
   render() {
     return (
       <section class="about" id="AboutSection">
@@ -10,35 +40,26 @@ export default class About extends Component {
             <span style={{ fontSize: "40px" }}>2.</span> About Me
             <div class="about-image"></div>
           </h2>
-          <div class="about-welcome">
-            <h3>More about me!</h3>
-            <p>
-              I'm an aspiring software developer that loves to program multiple
-              interesting apps to showcase to users.
-            </p>
-          </div>
-          <div class="about-goals">
-            <h3>Goals</h3>
-            <p>
-              My goal is to become a reliable programmer that create multiple
-              things that users/consumers enjoy.
-            </p>
-          </div>
-          <div class="about-education">
-            <h3>Education</h3>
-            <p>
-              I got my associate and bachelor's degree in computer science. I
-              graduated from The University of Tennessee, Knoxville in December
-              of 2019. Go Vols!
-            </p>
-          </div>
-          <div class="about-TBD">
-            <h3>TBD</h3>
-            <p>
-              Integer vehicula justo vel sapien viverra congue. Etiam efficitur
-              In scelerisque porta dictum. Suspendisse condimentum finibus eros,
-            </p>
-          </div>
+          <AboutCard
+            class={this.state.welcome.class}
+            title={this.state.welcome.title}
+            text={this.state.welcome.text}
+          />
+          <AboutCard
+            class={this.state.goals.class}
+            title={this.state.goals.title}
+            text={this.state.goals.text}
+          />
+          <AboutCard
+            class={this.state.education.class}
+            title={this.state.education.title}
+            text={this.state.education.text}
+          />
+          <AboutCard
+            class={this.state.tbd.class}
+            title={this.state.tbd.title}
+            text={this.state.tbd.text}
+          />
         </div>
       </section>
     );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import "./styles/index.css";
-//import Social from "./components/Social";
-//import Loading from "./components/Loading";
 import Header from "./components/Header";
 import Loading from "./components/Loading";
 import Welcome from "./components/Welcome";
 import About from "./components/About";
+import Social from "./components/Social";
 
 class App extends Component {
   constructor(props) {
@@ -26,7 +25,8 @@ class App extends Component {
       return (
         <div style={{ disply: "flex" }} id="HomeSection">
           <Header active={this.state.active} />
-          <div style={{ width: "100%", height: "2000px" }}>
+          <Social />
+          <div style={{ width: "100%", minHeight: "2000px" }}>
             <Welcome />
             <About />
           </div>
