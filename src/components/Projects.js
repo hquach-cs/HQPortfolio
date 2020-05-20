3b5f04431906e6c7a24d6b0c18b0778d775ba2a6
hquach-cs <hquach.cs@gmail.com>
2020-05-19 20:04:52 -0500

    Placeholder project card

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -23,10 +23,46 @@ export default class Projects extends Component {
               </h2>
               <div class="projects-main">
                 <div class="photo-grid">
+                  <a
+                    class="card project-Eco"
+                    href="https://github.com/hquach-cs/EcosystemSimulation"
+                  >
+                    <div class="project-content">
+                      <h4>Ecosystem Simulation (WIP)</h4>
+                      <p>
+                        A simulation of a fox and rabbit ecoystem. This project
+                        is a study of genetic algorithm.
+                      </p>
+                    </div>
+                  </a>
+                  <a
+                    class="card card-wide project-Portfolio"
+                    href="https://github.com/hquach-cs/HQPortfolio"
+                  >
+                    <div class="project-content">
+                      <h4>Portfolio Website</h4>
+                      <p>
+                        This was a project to create a portfolio website. For
+                        this project, I learn how to use React.js/HTML/CSS to
+                        create a responsive website.
+                      </p>
+                    </div>
+                  </a>
                   <div class="card"></div>
-                  <div class="card card-wide"></div>
-                  <div class="card"></div>
-                  <div class="card card-tall"></div>
+                  <a
+                    class="card card-tall project-GOL"
+                    href="https://hquach-cs.github.io/GameOfLife/"
+                  >
+                    <div class="project-content">
+                      {" "}
+                      <h4>Conway's Game of Life</h4>
+                      <p>
+                        This project was to create a visulization of a cellular
+                        automata rules created by John Horton Conway. Conway
+                        called this "Game of Life".
+                      </p>
+                    </div>
+                  </a>
                   <div class="card"></div>
                   <div class="card"></div>
                   <div class="card"></div>
